2665bfd7ae9f45c49afff6e0f2a3622adb54fd6f
Akijunior <suitsu19@gmail.com>
2018-03-20 15:17:01 -0300

    Correção do bug de js.

--- active_gears/urls.py
+++ active_gears/urls.py
@@ -8,16 +8,11 @@ from django.views.i18n import JavaScriptCatalog
 from django.conf.urls.static import static
 from django.conf import settings
 
-js_info_dict = {
-    'domain': 'djangojs',
-    'packages': ('wrnpro', ),
-}
-
 admin.autodiscover()
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
-    path('jsi18n/',  JavaScriptCatalog.as_view(packages=[js_info_dict]), name='javascript-catalog'),
+    path('jsi18n/',  JavaScriptCatalog.as_view(), name='javascript-catalog'),
     path('rosetta/', include('rosetta.urls')),
 
     path('admin/', admin.site.urls),
