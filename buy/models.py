f61eeb582a8228bf5a182aae199a3e60ce22a29c
Akijunior <suitsu19@gmail.com>
2018-03-21 01:00:47 -0300

    Isso é tudo, pessoal.

--- buy/models.py
+++ buy/models.py
@@ -3,24 +3,32 @@ from core.models import Gear
 
 from django.conf import settings
 
+
 class Buy(models.Model):
-	total_value = models.DecimalField(max_digits=5, decimal_places=2)
-	buyer = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Comprador', related_name='compras', on_delete=models.CASCADE)
+    buyer = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Comprador', related_name='purshases',
+                              on_delete=models.CASCADE)
+
+class Item(models.Model):
+    buy = models.ForeignKey('Buy', on_delete=models.CASCADE)
+    item = models.ForeignKey('core.Gear', verbose_name='peça', related_name='items', on_delete=models.CASCADE)
+
 
 class Pay(models.Model):
-	TYPE_PAY_CHOICES = (
-		('cartao', 'Cartão'),
-		('em especie', 'Em espécie'),
-		('por boleto', 'Por boleto'),
-	)
-	type_pay = models.CharField(max_length=20, choices=TYPE_PAY_CHOICES, default='Em especie')
-	total_value = models.DecimalField(max_digits=5, decimal_places=2)
-	buy = models.ForeignKey(Buy, verbose_name='Compra', related_name='pagamento', on_delete=models.CASCADE)
+    TYPE_PAY_CHOICES = (
+        ('cartao', 'Cartão'),
+        ('em especie', 'Em espécie'),
+        ('por boleto', 'Por boleto'),
+    )
+    type_pay = models.CharField(max_length=20, choices=TYPE_PAY_CHOICES, default='Em especie')
+    total_value = models.DecimalField(max_digits=5, decimal_places=2)
+    buy = models.ForeignKey(Buy, verbose_name='Compra', related_name='pagamento', on_delete=models.CASCADE)
+    date_pay = models.DateTimeField('Data de pagamento', auto_now_add=True)
+
 
 class Order(models.Model):
-	requester = models.ForeignKey(
-		settings.AUTH_USER_MODEL, verbose_name='solicitante',
-		related_name='solicitacoes', on_delete=models.CASCADE
-	)
-	item = models.ForeignKey(Gear, verbose_name='peça', related_name='solicitacoes', on_delete=models.CASCADE)
-	date_order = models.DateTimeField(auto_now_add=True)
+    requester = models.ForeignKey(
+        settings.AUTH_USER_MODEL, verbose_name='solicitante',
+        related_name='solicitacoes', on_delete=models.CASCADE
+    )
+    item = models.ForeignKey('core.Gear', verbose_name='peça', related_name='solicitacoes', on_delete=models.CASCADE)
+    date_order = models.DateTimeField(auto_now_add=True)
